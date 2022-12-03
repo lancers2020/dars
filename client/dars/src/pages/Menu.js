f6ca8b1b8503d50eb03facc503ed4cf80a31019c
ennaesor <sherylvelasco340@gmail.com>
2022-12-03 15:41:45 -0300

    testing

--- client/dars/src/pages/Menu.js
+++ client/dars/src/pages/Menu.js
@@ -2,9 +2,32 @@ import React from 'react'
 
 function Menu() {
   return (
-    <div>
-      Menu
+    <div className="container" id="navbar">
+     <div className='container-fluid'>
+      <img className=''></img>
+
+     </div>
+
+     <div className='row'>
+      <div className='col-sm-4 center'>
+        <img className=''></img>
+
+      </div>
+
+     </div>
+
+     <div className='col-sm-4 center'>
+      <img className=''></img>
+
+     </div>
+
+     <div className='col-sm-4 right'>
+      <img className=''></img>
+
+     </div>
     </div>
+
+
   )
 }
 
