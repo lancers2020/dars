881b41454d7573899a855065a8adf4a00b1c8efe
ennaesor <sherylvelasco340@gmail.com>
2022-12-04 17:41:44 -0300

    added pics

--- client/dars/src/pages/Menu.js
+++ client/dars/src/pages/Menu.js
@@ -1,41 +1,48 @@
 import React from 'react'
 import Card from "../components/Card";
 import CauliFlower from "../pictures/cauliflower.jpg";
+import adobo_mexican from "../pictures/adobo_mexican.jpg";
+import Lechon_pork from "../pictures/lechon_pork.jpg";
+import lechon_chicken from "../pictures/lechon_chicken_2.jpg";
+import pancit_palabok from "../pictures/pancit_palabok.jpg";
+import sisig from "../pictures/sisig.jpg";
 
 function Menu() {
   return (
     <div>
       <div className="container" id="navbar">
      <div className='container-fluid'>
-      <Card height="auto" width="200px" name="mango shake" bgColor={"red"} price="500 php" picture={CauliFlower}/>
+      <Card height="auto" width="300px" name="Cauliflower" bgColor={"violet"} price="100 php" picture={CauliFlower}/>
      </div>
 
      <div className='row'>
-      <div className='col-sm-4 center'>
-        <img className=''></img>
-        text 2
+      <div className='col-sm-4 my-5'>
+      <Card height="auto" width="300px" name="Adobo mexican" bgColor={"violet"} price="300 php" picture={adobo_mexican}/>
+      
       </div>
 
      </div>
 
      <div className='col-sm-4 center'>
-      <img className=''></img>
-      text 3
+     <Card height="auto" width="300px" name="Lechon pork" bgColor={"violet"} price="500 php" picture={Lechon_pork}/>
+     
      </div>
 
-     <div className='col-sm-4 right'>
-      <img className=''></img>
-      text 4
+     <div className='col-sm-4 center'>
+     <Card height="auto" width="300px" name="Lechon chicken" bgColor={"violet"} price="500 php" picture={lechon_chicken}/>
+ 
      </div>
     </div>
        <div className="col-6 center">
-         <img className="img-responsive img-thumbnail" src="" 
-         />
-       </div>
-
+       <Card height="auto" width="300px" name="Pancit palabok" bgColor={"violet"} price="250 php" picture={pancit_palabok}/>" 
+        
+       </div>   
+  
    
-   </div>    
-   
+     <div className='col-6 center'>
+      <Card height="auto" width="300px" name="Sisig" bgColor={"violet"} price="355 php" picture={sisig}></Card>
+     </div>
+    </div> 
   )
  } 
  
