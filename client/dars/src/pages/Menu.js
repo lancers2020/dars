f2b6f403623c9ec02a019e73ad257d83ded3a2ca
ennaesor <sherylvelasco340@gmail.com>
2022-12-05 21:27:48 -0300

    menu updated

--- client/dars/src/pages/Menu.js
+++ client/dars/src/pages/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from "../components/Card";
-import CauliFlower from "../pictures/cauliflower.jpg";
+import roasted_cauliFlower from "../pictures/roasted_cauliflower.jpg";
 import adobo_mexican from "../pictures/adobo_mexican.jpg";
 import Lechon_pork from "../pictures/lechon_pork.jpg";
 import lechon_chicken from "../pictures/lechon_chicken_2.jpg";
@@ -30,116 +30,114 @@ function Menu() {
 
     <div>
       <div className='banner'>
-       <div className='container-flex'>
-        <div className='img'>
-         <div className='center topMargin'>
-         <Card height="auto" width="auto" picture={banner}/>
-         <div className='banner-title-wrapper mt-5'>
-         </div>
-        </div>
+          <img src={banner} alt="banner" width="100%" height="100%"/>
+      <div className='banner-title-wrapper mt-5'>
+    </div>     
 
       <div className="container" id="navbar">
-       <div className='container-fluid'>
-        <h3>Main Course</h3>
-         <Card height="auto" width="300px" name="Cauliflower" bgColor={"violet"} price="100 php" picture={CauliFlower}/>
-       </div>
-     
-      <div className='row'>
-       <div className='col-6 sm-4'>
-        <Card height="auto" width="300px" name="Adobo mexican" bgColor={"violet"} price="300 php" picture={adobo_mexican}/>      
-       </div>
+           
+      <section id="section-1">
+        <h3>Main Dish</h3>
+      <div className="main-dish">
+      <div>
+        <Card height="200px" width="200px" name="Roasted cauliflower" bgColor={"violet"} price="100 php" picture={roasted_cauliFlower}/> 
+      </div>
+      <div className='col-6 sm-4'>
+        <Card height="200px" width="200px" name="Adobo mexican" bgColor={"violet"} price="300 php" picture={adobo_mexican}/>      
       </div>
     
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Lechon pork" bgColor={"violet"} price="455 php" picture={Lechon_pork}/>
+       <Card height="200px" width="200px" name="Lechon pork" bgColor={"violet"} price="455 php" picture={Lechon_pork}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Lechon chicken" bgColor={"violet"} price="499 php" picture={lechon_chicken}/> 
+       <Card height="200px" width="200px" name="Lechon chicken" bgColor={"violet"} price="499 php" picture={lechon_chicken}/> 
       </div>
   
       <div className="col-6 sm-4 center">
-       <Card height="auto" width="300px" name="Pancit palabok" bgColor={"violet"} price="250 php" picture={pancit_palabok}/>        
+       <Card height="200px" width="200px" name="Pancit palabok" bgColor={"violet"} price="250 php" picture={pancit_palabok}/>        
       </div>   
     
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Sisig" bgColor={"violet"} price="155 php" picture={sisig}/>
+       <Card height="200px" width="200px" name="Sisig" bgColor={"violet"} price="155 php" picture={sisig}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Beef salpicao" bgColor={"violet"} price="299 php" picture={beef_salpicao}/>
+       <Card height="200px" width="200px" name="Beef salpicao" bgColor={"violet"} price="299 php" picture={beef_salpicao}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Bistek tagalog" bgColor={"violet"} price="199 php" picture={bistek_tagalog}/>
+       <Card height="200px" width="200px" name="Bistek tagalog" bgColor={"violet"} price="199 php" picture={bistek_tagalog}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Bulalo" bgColor={"violet"} price="355 php" picture={bulalo}/>
+       <Card height="200px" width="200px" name="Bulalo" bgColor={"violet"} price="355 php" picture={bulalo}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Crispy pata" bgColor={"violet"} price="399 php" picture={crispy_pata}/>
+       <Card height="200px" width="200px" name="Crispy pata" bgColor={"violet"} price="399 php" picture={crispy_pata}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Dinuguan" bgColor={"violet"} price="199 php" picture={dinuguan}/>
+       <Card height="200px" width="200px" name="Dinuguan" bgColor={"violet"} price="199 php" picture={dinuguan}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Ginataang gulay" bgColor={"violet"} price="55 php" picture={ginataang_gulay}/>
+       <Card height="200px" width="200px" name="Ginataang gulay" bgColor={"violet"} price="55 php" picture={ginataang_gulay}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Humba" bgColor={"violet"} price="139 php" picture={humba}/>
+       <Card height="200px" width="200px" name="Humba" bgColor={"violet"} price="139 php" picture={humba}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Kaldereta" bgColor={"violet"} price="140 php" picture={kaldereta}/>
+       <Card height="200px" width="200px" name="Kaldereta" bgColor={"violet"} price="140 php" picture={kaldereta}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Inihaw na pusit" bgColor={"violet"} price="355 php" picture={inihaw_na_pusit}/>
+       <Card height="200px" width="200px" name="Inihaw na pusit" bgColor={"violet"} price="355 php" picture={inihaw_na_pusit}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Kare-kare" bgColor={"violet"} price="139 php" picture={kare_kare}/>
+       <Card height="200px" width="200px" name="Kare-kare" bgColor={"violet"} price="139 php" picture={kare_kare}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Lechon kawali" bgColor={"violet"} price="250 php" picture={lechon_kawali}/>
+       <Card height="200px" width="200px" name="Lechon kawali" bgColor={"violet"} price="250 php" picture={lechon_kawali}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Pancit guisado" bgColor={"violet"} price="299 php" picture={pancit_guisado}/>
+       <Card height="200px" width="200px" name="Pancit guisado" bgColor={"violet"} price="299 php" picture={pancit_guisado}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="pinapaitan" bgColor={"violet"} price="240 php" picture={pinapaitan}/>
+       <Card height="200px" width="200px" name="pinapaitan" bgColor={"violet"} price="240 php" picture={pinapaitan}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Pinaputok na isda" bgColor={"violet"} price="170 php" picture={pinaputok_na_isda}/>
+       <Card height="200px" width="200px" name="Pinaputok na isda" bgColor={"violet"} price="170 php" picture={pinaputok_na_isda}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Sizzling teriyaki squid" bgColor={"violet"} price="125 php" picture={sizzling_teriyaki_squid}/>
+       <Card height="200px" width="200px" name="Sizzling teriyaki squid" bgColor={"violet"} price="125 php" picture={sizzling_teriyaki_squid}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Sticky garlic chicken wings" bgColor={"violet"} price="180 php" picture={sticky_garlic_chicken_wings}/>
+       <Card height="200px" width="200px" name="Sticky garlic chicken wings" bgColor={"violet"} price="180 php" picture={sticky_garlic_chicken_wings}/>
       </div>
 
       <div className='col-6 sm-4 center'>
-       <Card height="auto" width="300px" name="Tostadong pork adobo" bgColor={"violet"} price="155 php" picture={tostadong_pork_adobo}/>
+       <Card height="200px" width="200px" name="Tostadong pork adobo" bgColor={"violet"} price="155 php" picture={tostadong_pork_adobo}/>
       </div>
+      </div>
+      </section>
+      
 
        
     </div> 
     </div>
    </div>
-  </div>
-</div>
+ 
 
   )
  } 
