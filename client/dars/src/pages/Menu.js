5699e7972854b8ba840d6510000edbcc89b0ad2e
unknown <sherylvelasco340@gmail.com>
2022-12-28 14:05:25 -0300

    menu coding done

--- client/dars/src/pages/Menu.js
+++ client/dars/src/pages/Menu.js
@@ -28,7 +28,95 @@ import sisig_with_rice from "../pictures/main_dish/sisig_with_rice.jpg";
 import sizzling_teriyaki_squid_with_rice from "../pictures/main_dish/sizzling_teriyaki_squid_with_rice.jpg";
 import tostadong_pork_with_rice from "../pictures/main_dish/tostadong_pork_with_rice.jpg";
 
+import bibingka from "../pictures/dessert/bibingka.jpg";
+import biko from "../pictures/dessert/biko.jpg";
+import buko_salad from "../pictures/dessert/buko_salad.jpg";
+import cassava_cake from "../pictures/dessert/cassava_cake.jpg";
+import ginataang_halohalo from "../pictures/dessert/ginataang_halohalo.jpg";
+import halo_halo from "../pictures/dessert/halo_halo.jpg";
+import kutsinta from "../pictures/dessert/kutsinta.jpg";
+import leche_flan from "../pictures/dessert/leche_flan.jpg";
+import maiz_con_yelo from "../pictures/dessert/maiz_con_yelo.jpg";
+import mamon_cake_cheddar_cheese from "../pictures/dessert/mamon_cake_cheddar_cheese.jpg";
+import minatamis_na_saging from "../pictures/dessert/minatamis_na_saging.jpg";
+import palitaw from "../pictures/dessert/palitaw.jpg";
+import sapin_sapin from "../pictures/dessert/sapin_sapin.jpg";
+import suman_malagkit from "../pictures/dessert/suman_malagkit.jpg";
+import taho from "../pictures/dessert/taho.jpg";
 
+import almond_milk_coffee from "../pictures/breakfast/almond_milk_coffee.jpg";
+import american_coffee from "../pictures/breakfast/american_coffee.jpg";
+import black_coffee from "../pictures/breakfast/black_coffee.jpg";
+import daing_na_bangus_with_fried_rice from "../pictures/breakfast/daing_na_bangus_with_fried_rice.jpg";
+import hot_cappuccino from "../pictures/breakfast/hot_cappuccino.jpg";
+import hot_chocolate from "../pictures/breakfast/hot_chocolate.jpg";
+import hot_latte from "../pictures/breakfast/hot_latte.jpg";
+import hot_milo from "../pictures/breakfast/hot_milo.jpg";
+import hot_mocha from "../pictures/breakfast/hot_mocha.jpg";
+import longganisa_egg_tuyo_with_rice from "../pictures/breakfast/longganisa_egg_tuyo_with_rice.jpg";
+import longganisa_with_rice_and_salted_egg from "../pictures/breakfast/longganisa_with_rice_and_salted_egg.jpg";
+import sisig_itlog_with_rice from "../pictures/breakfast/sisig_itlog_with_rice.jpg";
+import talong_itlog_with_rice from "../pictures/breakfast/talong_itlog_with_rice.jpg";
+import tapsilog from "../pictures/breakfast/tapsilog.jpg";
+
+import absolut_vodka from "../pictures/drinks/absolut_vodka.jpg";
+import banana_shake from "../pictures/drinks/banana_shake.jpg";
+import bear_beer from "../pictures/drinks/bear_beer.jpg";
+import becker from "../pictures/drinks/becker.jpg";
+import budlight from "../pictures/drinks/budlight.jpg";
+import budweiser from "../pictures/drinks/budweiser.jpg";
+import chivas_regal from "../pictures/drinks/chivas_regal.jpg";
+import cocacola from "../pictures/drinks/cocacola.jpg";
+import corona from "../pictures/drinks/corona.jpg";
+import escudo_silver from "../pictures/drinks/escudo_silver.jpg";
+import fanta_grapes from "../pictures/drinks/fanta_grapes.jpg";
+import fanta from "../pictures/drinks/fanta.jpg";
+import ginebra_san_miguel from "../pictures/drinks/ginebra_san_miguel.jpg";
+import heineken from "../pictures/drinks/heineken.jpg";
+import lemon_juice from "../pictures/drinks/lemon_juice.jpg";
+import london_dry_gin from "../pictures/drinks/london_dry_gin.jpg";
+import melon_juice from "../pictures/drinks/melon_juice.jpg";
+import orange_juice from "../pictures/drinks/orange_juice.jpg";
+import pepsi from "../pictures/drinks/pepsi.jpg";
+import pineapple_juice from "../pictures/drinks/pineapple_juice.jpg";
+import popcola from "../pictures/drinks/popcola.jpg";
+import redhorse from "../pictures/drinks/redhorse.jpg";
+import sprite from "../pictures/drinks/sprite.jpg";
+import strawberry_shake from "../pictures/drinks/strawberry_shake.jpg";
+import tanduay_ice from "../pictures/drinks/tanduay_ice.jpg";
+import tanduay from "../pictures/drinks/tanduay.jpg";
+import tiger_beer from "../pictures/drinks/tiger_beer.jpg";
+import watermelon_juice from "../pictures/drinks/watermelon_juice.jpg";
+
+import atchara from "../pictures/appetizer/atchara.jpg";
+import beef_empanada from "../pictures/appetizer/beef_empanada.jpg";
+import cheese_stick from "../pictures/appetizer/cheese_stick.jpg";
+import chicharon from "../pictures/appetizer/chicharon.jpg";
+import crispy_tenga from "../pictures/appetizer/crispy_tenga.jpg";
+import deep_fried_chicken_skin from "../pictures/appetizer/deep_fried_chicken_skin.jpg";
+import dynamite_lumpia from "../pictures/appetizer/dynamite_lumpia.jpg";
+import fishball from "../pictures/appetizer/fishball.jpg";
+import fried_lumpia from "../pictures/appetizer/fried_lumpia.jpg";
+import kwek_kwek from "../pictures/appetizer/kwek_kwek.jpg";
+import lumpiang_shanghai from "../pictures/appetizer/lumpiang_shanghai.jpg";
+import okoy from "../pictures/appetizer/okoy.jpg";
+import pork_siomai from "../pictures/appetizer/pork_siomai.jpg";
+
+import adobong_mani from "../pictures/pulutan/adobong_mani.jpg";
+import bagnet from "../pictures/pulutan/bagnet.jpg";
+import bopis from "../pictures/pulutan/bopis.jpg";
+import buffalo_wings from "../pictures/pulutan/buffalo_wings.jpg";
+import gambas from "../pictures/pulutan/gambas.jpg";
+import isaw from "../pictures/pulutan/isaw.jpg";
+import kilawing_isda_at_baboy from "../pictures/pulutan/kilawing_isda_at_baboy.jpg";
+import kilawing_isda from "../pictures/pulutan/kilawing_isda.jpg";
+import nilasing_na_hipon from "../pictures/pulutan/nilasing_na_hipon.jpg";
+import pigar_pigar from "../pictures/pulutan/pigar_pigar.jpg";
+import pork_barbecue from "../pictures/pulutan/pork_barbecue.jpg";
+import sizzling_tufo from "../pictures/pulutan/sizzling_tufo.jpg";
+import spicy_chicken_wings from "../pictures/pulutan/spicy_chicken_wings.jpg";
+import spicy_fried_calamari from "../pictures/pulutan/spicy_fried_calamari.jpg";
+import tokwat_baboy from "../pictures/pulutan/tokwat_baboy.jpg";
 import banner from "../pictures/logo/banner.jpg";
 import OverFlowX from './components/OverFlowX';
 
@@ -59,6 +147,106 @@ const MenuDisplay=()=>{
     "sinigang na baboy with rice",
     "sisig with rice"
    ]
+
+    var label_dessert=[
+     "bibingka",
+     "biko",
+     "buko salad",
+     "cassava cake",
+     "ginataang halohalo",
+     "halo halo",
+     "kutsinta",
+     "leche flan",
+     "maiz con yelo",
+     "mamon cake cheddar cheese",
+     "minatamis na saging",
+     "palitaw",
+     "sapin sapin",
+     "suman malagkit",
+     "taho"
+    ]
+
+    var label_breakfast=[
+        "almond milk coffee",
+        "american coffee",
+        "black coffee",
+        "daing na bangus with fried rice",
+        "hot cappuccino",
+        "hot chocolate",
+        "hot latte",
+        "hot milo",
+        "hot mocha",
+        "longganisa egg tuyo with rice",
+        "longganisa with rice and salted egg",
+        "sisig itlog with rice",
+        "talong itlog with rice",
+        "tapsilog"
+    ]
+
+    var label_drinks=[
+        "absolut vodka",
+        "banana shake",
+        "bear beer",
+        "becker",
+        "budlight",
+        "budweiser",
+        "chivas regal",
+        "cocacola",
+        "corona",
+        "escudo silver",
+        "fanta grapes",
+        "fanta",
+        "ginebra san miguel",
+        "heineken",
+        "lemon juice",
+        "london dry gin",
+        "melon juice",
+        "orange juice",
+        "pepsi",
+        "pineapple juice",
+        "popcola",
+        "redhorse",
+        "sprite",
+        "strawberry shake",
+        "tanduay ice",
+        "tanduay",
+        "tiger beer",
+        "watermelon juice"
+    ]
+
+    var label_appetizer=[
+        "atchara",
+        "beef empanada",
+        "cheese stick",
+        "chicharon",
+        "crispy tenga",
+        "deep fried chicken skin",
+        "dynamite lumpia",
+        "fishball",
+        "fried lumpia",
+        "kwek kwek",
+        "lumpiang shanghai",
+        "okoy",
+        "pork siomai"
+    ]
+
+    var label_pulutan=[
+        "adobong mani",
+        "bagnet",
+        "bopis", 
+        "buffalo wings",
+        "gambas",
+        "isaw",
+        "kilawing isda at baboy",
+        "kilawing isda",
+        "nilasing na hipon",
+        "pigar pigar",
+        "pork barbecue",
+        "sizzling tufo",
+        "spicy chicken wings",
+        "spicy fried calamari",
+        "tokwat baboy"
+    ]
    
    var pictures_main_dish = [
     arroz_caldo,
@@ -89,13 +277,121 @@ const MenuDisplay=()=>{
     tostadong_pork_with_rice
    ]
 
+   var pictures_dessert = [
+    bibingka,
+    biko,
+    buko_salad,
+    cassava_cake,
+    ginataang_halohalo,
+    halo_halo,
+    kutsinta,
+    leche_flan,
+    maiz_con_yelo,
+    mamon_cake_cheddar_cheese,
+    minatamis_na_saging,
+    palitaw,
+    sapin_sapin,
+    suman_malagkit,
+    taho
+   ]
+
+   var pictures_breakfast = [
+    almond_milk_coffee,
+    american_coffee,
+    black_coffee,
+    daing_na_bangus_with_fried_rice,
+    hot_cappuccino,
+    hot_chocolate,
+    hot_latte,
+    hot_milo,
+    hot_mocha,
+    longganisa_egg_tuyo_with_rice,
+    longganisa_with_rice_and_salted_egg,
+    sisig_itlog_with_rice,
+    talong_itlog_with_rice,
+    tapsilog
+   ]
+
+   var pictures_drinks =[
+     absolut_vodka,
+     banana_shake,
+     bear_beer,
+     becker,
+     budlight,
+     budweiser,
+     chivas_regal,
+     cocacola,
+     corona,
+     escudo_silver,
+     fanta_grapes,
+     fanta,
+     ginebra_san_miguel,
+     heineken,
+     lemon_juice,
+     london_dry_gin,
+     melon_juice,
+     orange_juice,
+     pepsi,
+     pineapple_juice,
+     popcola,
+     redhorse,
+     sprite,
+     strawberry_shake,
+     tanduay_ice,
+     tanduay,
+     tiger_beer,
+     watermelon_juice
+   ]
+
+   var pictures_appetizer =[
+     atchara,
+     beef_empanada,
+     cheese_stick,
+     chicharon,
+     crispy_tenga,
+     deep_fried_chicken_skin,
+     dynamite_lumpia,
+     fishball,
+     fried_lumpia,
+     kwek_kwek,
+     lumpiang_shanghai,
+     okoy,
+     pork_siomai
+   ]
+
+   var pictures_pulutan =[
+     adobong_mani,
+     bagnet,
+     bopis,
+     buffalo_wings,
+     gambas,
+     isaw,
+     kilawing_isda_at_baboy,
+     kilawing_isda,
+     nilasing_na_hipon,
+     pigar_pigar,
+     pork_barbecue,
+     sizzling_tufo,
+     spicy_chicken_wings,
+     spicy_fried_calamari,
+     tokwat_baboy
+   ]
+
+
   const Breakfast=()=>{
       if(isBreakfast===true){
           return (
               <div
                  style={{}}
               >
-                 <h1>breakfast</h1>
+                <OverFlowX 
+                    category="Breakfast"
+                    label={label_breakfast}
+                    width="200px" 
+                    height="200px" 
+                    bgColor="pink"
+                    picture={pictures_breakfast}
+                 />
               </div>
           )
       }
@@ -107,7 +403,14 @@ const MenuDisplay=()=>{
               <div
                 style={{}}
               >
-                 <h1>appetizer</h1>     
+                  <OverFlowX 
+                    category="Appetizer"
+                    label={label_appetizer}
+                    width="200px" 
+                    height="200px" 
+                    bgColor="pink"
+                    picture={pictures_appetizer}  
+                /> 
               </div>
          )
       }          
@@ -142,11 +445,11 @@ const MenuDisplay=()=>{
               >
                  <OverFlowX
                   category="Dessert"
-                  label={label_main_dish}
+                  label={label_dessert}
                   width="200px" 
                   height="200px" 
                   bgColor="pink"
-                  picture={pictures_main_dish}
+                  picture={pictures_dessert}
                  />
               </div>               
           )
@@ -159,7 +462,14 @@ const MenuDisplay=()=>{
               <div
                 style={{}}
               >
-                 <h1>pulutan</h1>
+                <OverFlowX 
+                    category="Pulutan"
+                    label={label_pulutan}
+                    width="200px" 
+                    height="200px" 
+                    bgColor="pink"
+                    picture={pictures_pulutan}
+                 />
               </div>             
           )
       }       
@@ -171,7 +481,14 @@ const MenuDisplay=()=>{
               <div
                  style={{}}
               >
-                 <h1>drinks</h1>
+                  <OverFlowX 
+                    category="Drinks"
+                    label={label_drinks}
+                    width="200px" 
+                    height="200px" 
+                    bgColor="pink"
+                    picture={pictures_drinks}
+                 />
               </div>
           )
       }
