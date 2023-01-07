9bebc9816baaf67d4a3be8cb344cd8fbbdcdf7ab
unknown <sherylvelasco340@gmail.com>
2023-01-07 00:05:02 -0300

    pic added

--- client/dars/src/pages/Menu.js
+++ client/dars/src/pages/Menu.js
@@ -6,11 +6,21 @@ import black_coffee from "../pictures/breakfast/black_coffee.jpg";
 import hot_chocolate from "../pictures/breakfast/hot_chocolate.jpg";
 import waffle_blueberries from "../pictures/breakfast/waffle_Blueberries.jpg";
 import waffle_strawberry from "../pictures/breakfast/waffle_Strawberry.jpg";
+import daing_with_rice from "../pictures/breakfast/daing_with_rice.jpg";
+import hot_milo from "../pictures/breakfast/hot_milo.jpg";
+import hot_mocha from "../pictures/breakfast/hot_mocha.jpg";
+import longganisa_with_rice from "../pictures/breakfast/longganisa_with_rice.jpg";
+import tocino_with_rice from "../pictures/breakfast/tocino_with_rice.jpg";
 
 import atchara from "../pictures/appetizer/atchara.jpg";
 import dynamite_lumpia from "../pictures/appetizer/dynamite_lumpia.jpg";
 import fishball from "../pictures/appetizer/fishball.jpg";
 import lumpiang_shanghai from "../pictures/appetizer/lumpiang_shanghai.jpg";
+import beef_empanada from "../pictures/appetizer/beef_empanada.jpg";
+import cheese_stick from "../pictures/appetizer/cheese_stick.jpg";
+import chicharon_bulaklak from "../pictures/appetizer/chicharon_bulaklak.jpg";
+import crispy_chicken_skin from "../pictures/appetizer/crispy_chicken_skin.jpg";
+import crispy_tenga from "../pictures/appetizer/crispy_tenga.jpg";
 
 import crispy_pata from "../pictures/main_dish/crispy_Pata.jpg";
 import dinuguan from "../pictures/main_dish/dinuguan.jpg";
@@ -18,11 +28,19 @@ import karekare from "../pictures/main_dish/karekare.jpg";
 import palabok from "../pictures/main_dish/palabok.jpg";
 import pancit_bijon from "../pictures/main_dish/pancit_bijon.jpg";
 import pinapaitan from "../pictures/main_dish/pinapaitan.jpg";
+import inihaw_na_pusit from "../pictures/main_dish/inihaw_na_pusit.jpg";
+import lechon_kawali from "../pictures/main_dish/lechon_kawali";
+import pritong_isda from "../pictures/main_dish/pritong_isda";
 
 import halo_halo from "../pictures/dessert/halo_halo.jpg";
 import kutsinta from "../pictures/dessert/kutsinta.jpg";
 import lecheflan from "../pictures/dessert/lecheflan.jpg";
 import maizcon_yelo from "../pictures/dessert/maizcon_Yelo.jpg";
+import bibingka  from "../pictures/dessert/bibingka.jpg";
+import biko from "../pictures/dessert/biko.jpg";
+import buko_salad from "../pictures/dessert/buko_salad.jpg";
+import ginataang_halohalo from "../pictures/dessert/ginataang_halohalo.jpg";
+import minatamis_na_saging from "../pictures/dessert/minatamis_na_saging.jpg";
 
 import heineken from "../pictures/drinks/heineken.jpg";
 import melon_juice from "../pictures/drinks/melon_juice.jpg";
@@ -30,7 +48,9 @@ import pineapple_juice from "../pictures/drinks/pineapple_juice.jpg";
 import red_horse from "../pictures/drinks/red_Horse.jpg";
 import strawberry_juice from "../pictures/drinks/strawberry_Juice.jpg";
 import tiger_beer from "../pictures/drinks/tiger_beer.jpg";
-
+import coca_cola from "../pictures/drinks/coca_cola.jpg";
+import corona from "../pictures/drinks/corona.jpg";
+import lemon_juice from "../pictures/drinks/lemon_juice.jpg";
 
 import banner from "../pictures/logo/banner.jpg";
 import OverFlowX from './components/OverFlowX';
@@ -38,42 +58,63 @@ import OverFlowY from './components/OverFlowY';
 
 const MenuDisplay=()=>{
   var label_main_dish=[
-    "crispy pata",
-    "dinuguan",
-    "karekare",
-    "palabok",
-    "pancit bijon",
-    "pinapaitan" 
+    "Crispy pata",
+    "Dinuguan",
+    "Kare kare",
+    "Palabok",
+    "Pancit bijon",
+    "Pinapaitan",
+    "Inihaw na pusit",
+    "Lechon kawali",
+    "pritong isda" 
   ]
 
   var label_dessert=[
-     "halo halo",
-     "kutsinta",
-     "lecheflan",
-     "maizcon Yelo"
+     "Halo halo",
+     "Kutsinta",
+     "Lecheflan",
+     "Maiz con Yelo",
+     "Bibingka",
+     "Biko",
+     "Buko salad",
+     "Ginataang halo halo",
+     "Minatamis na saging"
   ]
 
   var label_breakfast=[
-        "black Coffee",
-        "hot Chocolate",
-        "waffle Blueberries",
-        "waffle Strawberry"
+        "Black Coffee",
+        "Hot Chocolate",
+        "Waffle Blueberries",
+        "Waffle Strawberry",
+        "Daing with rice",
+        "Hot milo",
+        "Hot mocha",
+        "Longganisa with rice",
+        "Tocino with rice"
   ]
 
     var label_drinks=[
-        "heineken",
-        "melon Juice",
-        "pineapple Juice",
-        "red Horse",
-        "strawberry Juice",
-        "tiger Beer"
+        "Heineken",
+        "Melon Juice",
+        "Pineapple Juice",
+        "Red Horse",
+        "Strawberry Juice",
+        "Tiger Beer",
+        "Coca cola",
+        "Corona",
+        "Lemon juice"
     ]
 
     var label_appetizer=[
-        "atchara",
-        "dynamite Lumpia",
-        "fishball",
-        "lumpiang Shanghai"
+        "Atchara",
+        "Dynamite lumpia",
+        "Fishball",
+        "Lumpiang shanghai",
+        "Beef empanada",
+        "Cheese stick",
+        "Chicharon bulaklak",
+        "Crispy chicken skin",
+        "crispy tenga"
     ]
 
     
@@ -84,21 +125,35 @@ const MenuDisplay=()=>{
        karekare,
        palabok,
        pancit_bijon,
-       pinapaitan
+       pinapaitan,
+       inihaw_na_pusit,
+       lechon_kawali,
+       pritong_isda
+
    ]
 
    var pictures_dessert = [
        halo_halo,
        kutsinta,
        lecheflan,
-       maizcon_yelo
+       maizcon_yelo,
+       bibingka,
+       biko,
+       buko_salad,
+       ginataang_halohalo,
+       minatamis_na_saging
    ]
 
    var pictures_breakfast = [
        black_coffee,
        hot_chocolate,
        waffle_blueberries,
-       waffle_strawberry
+       waffle_strawberry,
+       daing_with_rice,
+       hot_milo,
+       hot_mocha,
+       longganisa_with_rice,
+       tocino_with_rice
    ]
 
    var pictures_drinks =[
@@ -107,14 +162,22 @@ const MenuDisplay=()=>{
        pineapple_juice,
        red_horse,
        strawberry_juice,
-       tiger_beer
+       tiger_beer,
+       coca_cola,
+       corona,
+       lemon_juice
    ]
 
    var pictures_appetizer =[
        atchara,
        dynamite_lumpia,
        fishball,
-       lumpiang_shanghai   
+       lumpiang_shanghai,
+       beef_empanada,
+       cheese_stick,
+       chicharon_bulaklak,
+       crispy_chicken_skin,
+       crispy_tenga   
    ]
 
 
