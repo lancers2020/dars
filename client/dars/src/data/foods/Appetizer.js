5f087129bb8098041a6907b2ab2811c90f80feb4
unknown <sherylvelasco340@gmail.com>
2023-01-08 00:31:40 -0300

    another try

--- client/dars/src/data/foods/Appetizer.js
+++ client/dars/src/data/foods/Appetizer.js
@@ -4,11 +4,11 @@ import React from 'react'
 function Appetizer(){
     const appetizer={
        "names":["Atchara", "Dynamite lumpia", "Fishball", "Lumpiang shanghai","Beef empanada",
-                "Cheese stick", "Chicharon bulaklak", "Crispy chicken skin", "crispy tenga"],
+                "Cheese stick", "Chicharon bulaklak", "Crispy chicken skin", "Crispy tenga"],
        "desc":["Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet",
                "Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet", 
                "Lorem ipsum dolor sit amet"],
-       "price":["9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php",]
+       "price":["10.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php", "9.99 php",]
  
     }
     const Result = () =>{
