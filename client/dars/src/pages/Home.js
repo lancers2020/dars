46762302ace783bd1fed991d24349aae5512a5f6
davelimutin <dave.limutin.pixel8@gmail.com>
2023-01-08 11:18:49 +0800

    removed some imports that aren't used

--- client/dars/src/pages/Home.js
+++ client/dars/src/pages/Home.js
@@ -5,7 +5,6 @@ import Appetizer from '../data/foods/Appetizer'
 import Drinks from '../data/foods/Drinks'
 import Dessert from '../data/foods/Dessert'
 import Breakfast from '../data/foods/Breakfast'
-import Login from '../test/sheryl/Testing2'
 
 function Home() {
   return (
--- client/dars/src/test/sheryl/testing2.js
+++ client/dars/src/test/sheryl/testing2.js
@@ -2,7 +2,6 @@ import React,{useState, Component} from "react"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import "../../pages/css/login.css"
-import Testing2 from "./Testing2"
 
 
 
