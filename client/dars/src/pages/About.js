f7cc4614bf80215475e77202623221ebed3d72f8
dzon caberte <dzonpsalmcaberte@gmail.com>
2023-01-10 09:57:06 -0500

    latest

--- client/dars/src/pages/About.js
+++ client/dars/src/pages/About.js
@@ -6,45 +6,30 @@ import './css/about.css'
 function About (){
   return(
     <div className="about">
-      <h1>About Us</h1>
-      <p>Hello, wishes from "name of the page"</p>
-      <br/>
-      <p>
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-      </p>
-      <br/>
-      <p>
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-       lorem lorem lorem lorem
-      </p>
-      <br/>
-  
+
+      <div>
+          <div className="container">
+              <img src="../pictures/logo/eiliv_aceron.jpg" width= "50px" height= "50px"/> 
+          </div>
+          <div className="about_details">
+            <span>About Our Restaurant</span>
+          </div>
+
+          
+
+      </div>
+
+
+
+    
       <h3>Browse through some of our videos</h3>
-      <br/>
-  
-     
+    
   <div className="parent">
     <div className="child">
       <div><iframe width="560" height="315" src="https://www.youtube.com/embed/nDpb7_44-RU" title="YouTube video player" border="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
       <div><iframe width="560" height="315" src="https://www.youtube.com/embed/i20HWCThzzw" title="YouTube video player" border="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
     </div>
-    <div className="child">
-      <div><iframe width="560" height="315" src="https://www.youtube.com/embed/gjrwVQyKKrk" title="YouTube video player" border="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
-      <div><iframe width="560" height="315" src="https://www.youtube.com/embed/ma2MkUt24Wo" title="YouTube video player" border="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
-    </div>
+
   </div>
 </div>
 )}
